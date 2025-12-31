d78abd5587c81ce8b9e26f8e38d428d8053ca69d
Stian Z. Vrba <stian.vrba@outlook.com>
2025-12-31 10:38:08 +0100

    Add test.

---
+++ Podaga.Test/InternableList_Test.cs
@@ -0,0 +1,11 @@
+﻿using Podaga.Interning;
+using System.Reflection;
+
+namespace Podaga.Test;
+
+internal class InternableList_Test
+{
+    private static readonly FieldInfo BackingListProperty = typeof(InternableList<int>).GetField("list", BindingFlags.NonPublic)!;
+
+
+}
